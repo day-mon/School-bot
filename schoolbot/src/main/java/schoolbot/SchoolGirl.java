9e46948ae64a5d5928eb8c46224a8e1822f75e3e
Elsklivet <55718878+Elsklivet@users.noreply.github.com>
2020-10-26 19:48:07 -0400

    fucking shit

--- schoolbot/src/main/java/schoolbot/SchoolGirl.java
+++ schoolbot/src/main/java/schoolbot/SchoolGirl.java
@@ -1,18 +1,42 @@
 package schoolbot;
 
-public class SchoolGirl extends ListenerAdapter
-{
-    public static void main(String[] args) throws LoginException
-    {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.*;
+import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.*;
+import net.dv8tion.jda.annotations.*;
+import net.dv8tion.jda.api.requests.*;
+import net.dv8tion.jda.api.events.channel.text.*;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.*;
+
+public class SchoolGirl extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException {
         if (args.length < 1) {
             System.out.println("You have to provide a token as first argument!");
             System.exit(1);
         }
+
+        try {
+            FileReader fr = new FileReader(new File("schoolbot\\src\\main\\files\\token.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Wow you're shit at coding");
+            e.printStackTrace();
+        }
+
         // args[0] should be the token
         // We only need 2 intents in this bot. We only respond to messages in guilds and private channels.
         // All other events will be disabled.
         JDABuilder.createLight(args[0], GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
-            .addEventListeners(new Bot())
+            .addEventListeners(new SchoolGirl())
             .setActivity(Activity.playing("Type !ping"))
             .build();
     }
@@ -21,12 +45,12 @@ public class SchoolGirl extends ListenerAdapter
     public void onMessageReceived(MessageReceivedEvent event)
     {
         Message msg = event.getMessage();
-        if (msg.getContentRaw().equals("!ping"))
+        if (msg.getContentRaw().equals("school.ping"))
         {
             MessageChannel channel = event.getChannel();
             long time = System.currentTimeMillis();
             channel.sendMessage("Pong!") /* => RestAction<Message> */
-                   .queue(response /* => Message */ -> {
+                   .queue(response -> {
                        response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
                    });
         }
