b12d73ef151f6095aa36272b49461381c5388a2c
Damon <lolmemedon@gmail.com>
2020-10-26 22:26:21 -0400

    added mroe stuff to classroom

--- schoolbot/src/main/java/schoolbot/Professor.java
+++ schoolbot/src/main/java/schoolbot/Professor.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class Professor {
 
     private HashMap<String, Classroom> professorsClasses;
+    private HashMap<Student, Classroom> studentsInClasses;
 
     Professor() {
 
