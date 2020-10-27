60e4a07de1903a5139a725c49b28ffc6d2141775
Elsklivet <55718878+Elsklivet@users.noreply.github.com>
2020-10-26 21:17:20 -0400

    throws declaration

--- schoolbot/src/main/java/schoolbot/StringOperations.java
+++ schoolbot/src/main/java/schoolbot/StringOperations.java
@@ -1,6 +1,5 @@
 package schoolbot;
 
-java.lang.Throwable;
 /** Basic String operations that will be useful for a bot.
  * @author Elsklivet#8867
  */
@@ -9,7 +8,7 @@ public class StringOperations{
      * @param str String to remove all quotations from
      * @return String without any quotation marks
      */
-    public static String removeQuotes(String str){
+    public static String removeQuotes(String str) throws Exception{
         if(str == null || str.length() <= 2) throw new Exception("String is not sized correctly to manipulate: "+str);
         return str.replaceAll("\\\"", "");
     }
@@ -18,7 +17,7 @@ public class StringOperations{
      * @param str String to remove all quotations from
      * @return String without any quotation marks
      */
-    public static String removeBounds(String str){
+    public static String removeBounds(String str) throws Exception{
         if(str == null || str.length() <= 2) throw new Exception("String is not sized correctly to manipulate: "+str);
         return str.substring(1,str.length()-1);
     }
