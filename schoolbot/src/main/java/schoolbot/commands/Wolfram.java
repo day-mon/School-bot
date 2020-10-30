618f4124303c93a37367579727a18a4b02ba1600
Damon <lolmemedon@gmail.com>
2020-10-29 22:20:33 -0400

    added stuffs

--- schoolbot/src/main/java/schoolbot/commands/Wolfram.java
+++ schoolbot/src/main/java/schoolbot/commands/Wolfram.java
@@ -1,5 +1,7 @@
 package schoolbot.commands;
 
+import java.util.HashMap;
+
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.MessageChannel;
@@ -32,6 +34,32 @@ public class Wolfram extends Command {
             
         new InvalidUsage("n.com", "n", "n", event.getMessage());
         } else {
+            args[0] = args[0].replaceAll("%", "%25")
+            .replaceAll("\\+", "%2B")
+            .replaceAll("\\s", "+")
+            .replaceAll("\\$", "%20")
+            .replaceAll("&", "%26")
+            .replaceAll("\\^", "%5E")
+            .replaceAll("`", "%60")
+            .replaceAll("\\/", "%2F")
+            .replaceAll("'", "%27")
+            .replaceAll("\\{", "%7B")
+            .replaceAll("\\}", "%7D")
+            .replaceAll(",", "%2C")
+            .replaceAll("\\\"", "%22")
+            .replaceAll("[“]", "%22")
+            .replaceAll("[”]", "%22")
+            .replaceAll("\\|", "%7C")
+            .replaceAll("=", "%3D")
+            .replaceAll(":", "%3A")
+            .replaceAll("<", "%3C")
+            .replaceAll("\\(", "%28")
+            .replaceAll("\\)", "%29")
+            .replaceAll(">", "%3E")
+            .replaceAll(";", "%3B")
+            .replaceAll("~", "%7E")
+            .replaceAll("@", "%40");
+
             channel.sendMessage("https://www.wolframalpha.com/input/?i=" + args[0]).queue();
 
         }
