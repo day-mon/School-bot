6c0232beeb953627dd3e6b8e7a5814d4e4e05baf
ryanl09 <67206661+ryanl09@users.noreply.github.com>
2021-02-08 20:54:19 -0500

    start listassignments

--- schoolbot/src/main/java/schoolbot/commands/school/ListAssignments.java
+++ schoolbot/src/main/java/schoolbot/commands/school/ListAssignments.java
@@ -3,7 +3,11 @@ package schoolbot.commands.school;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import schoolbot.Ryan;
 import schoolbot.commands.Command;
+import schoolbot.natives.Assignment;
+import schoolbot.natives.Classroom;
+import schoolbot.natives.School;
 
 public class ListAssignments extends Command {
 
@@ -21,9 +25,29 @@ public class ListAssignments extends Command {
         Member userTyping = event.getMember();
         boolean adminCheck = userTyping.hasPermission(Permission.ADMINISTRATOR);
 
-        if ()
+        if (adminCheck || 1 == 1) {
+            if (args.length == 2) {
+                String schoolKey = args[0];
+                String classId = args[1];
+                School s = Ryan.schools.get(schoolKey);
+                Classroom c = s.getListOfClasses().get(classId);
+                String print = "";
+                String chan = "bot-test-grounds";
+                for (Assignment a : c.getAssignments().values()) {
+                    print += a.toString() + "\n";
+                }
 
+                if (!print.equals("")) {
+                    Ryan.jda.getTextChannelsByName(chan, true).get(0).sendMessage(print);
+                } else {
+                    // "There are currently no assignments for this class"
+                }
+            } else {
+                // invalid number of args
+            }
+
+        }
 
     }
-    
+
 }
